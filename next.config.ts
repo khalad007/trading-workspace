4efc3a2436e583deac1abcaf351d28b9c0cd0044
MD. KHALAD SHIFULLAH <www.arifhossain49919@gmail.com>
2026-08-30 18:21:17 +0600

    Use standard Prisma client for Vercel

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  outputFileTracingIncludes: {
-    "/*": [
-      "./src/generated/prisma/libquery_engine-rhel-openssl-3.0.x.so.node",
-    ],
-  },
   images: {
     remotePatterns: [
       {
